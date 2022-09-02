7f10ea325b2840692a08ae10aa4212fd22c4ac38
Abdulqadir <abdulqadirlalparia@gmail.com>
2022-09-02 20:57:32 +0530

    Added logic for Rock paper scissor and etc.

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function GetComputerChoice(){
     let randindex =Math.floor(Math.random()* choices.length);
     return choices[randindex]
 }
+
+
+function round(playerSelection , computerSelection){
+    
+    if (computerSelection === "rock" && playerSelection === "paper"){
+        return ('You win! Paper beats Rock.')
+    } else if( computerSelection === "paper" && playerSelection === "rock"){
+        return ('You lose! paper beats rock.')
+    }else if (computerSelection === "paper" && playerSelection === "scissors"){
+        return('You win! scissors beats paper.')
+    }else if (computerSelection === "scissors" && playerSelection === "paper"){
+        return('You lose! Scissors beats paper.')
+    }else if (computerSelection === "scissors" && playerSelection === "rock"){
+        return('You win! rock beats scissors.')
+    }else if (computerSelection === "rock" && playerSelection === "scissors"){
+        return('You lose! rock beats scissors')
+    }else{
+        return "Draw";
+    }
+}
+
+
+for(let i = 0; i < 5; i++){
+    let computerSelection = GetComputerChoice();
+let playerSelection = prompt("Enter");
+console.log(playerSelection + ' vs ' + computerSelection)
+    console.log(round(playerSelection, computerSelection));
+}
